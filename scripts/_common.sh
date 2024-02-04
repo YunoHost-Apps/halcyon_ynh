f4cdfc11a30cad1456af577957730c769560ba73
Félix Piédallu <felix@piedallu.me>
2024-02-04 18:59:58 +0100

    Packaging v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,14 +3,6 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
-# PHP APP SPECIFIC
-#=================================================
-
-YNH_PHP_VERSION="7.4"
-php_dependencies="php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-gettext"
-
-# dependencies used by the app (must be on a single line)
-pkg_dependencies="$php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
